c19691c464d49651e20a51ffc8a8a726f22dc1e0
Stachu1 <stach.basiuk@gmail.com>
2024-12-10 15:33:42 +0100

    Added IR LED sensor rps measuring

--- src/main.c
+++ src/main.c
@@ -10,15 +10,19 @@
 #define VOLTAGE_DIVIDER_RATIO 3.0f                                      // 1:3 voltage divider
 #define CURRENT_SENSOR_GAIN 2.7027028f                                  // 2.7027028 A/V
 #define CURRENT_SENSOR_OFFSET -1.12f                                    // -1.12 A
-#define UNDERVOLTAGE_THRESHOLD -9.6f                                     // 9.6 V
+#define UNDERVOLTAGE_THRESHOLD 9.6f                                     // 9.6 V
 #define OVERCURRENT_THRESHOLD 10.0f                                     // 10.0 A
+#define IR_LED_PIN PB5                                                  // PD 13
+#define INTERRUPT_PIN PD2                                               // PD2(INT0)
 
 
 volatile uint16_t adc0_value = 0;
 volatile uint16_t adc7_value = 0;
 volatile uint8_t current_channel = 0;
 volatile uint32_t millis = 0;
-float rps = 0;
+volatile uint32_t last_rotation = 0;
+volatile uint16_t rps_led = 0;
+int16_t rps_esc = 0;
 
 void adc_init(void) {
     // AREF = AVcc
@@ -43,6 +47,14 @@ void timer1_init(void) {
 }
 
 
+// Set up external interrupt on PD2 (INT0)
+void interrupt_init(void) {
+    // Configure INT0 (PD2) to trigger on falling edge
+    EICRA |= (1 << ISC01);   // Set ISC01 to 1 (falling edge)
+    EIMSK |= (1 << INT0);    // Enable INT0 interrupt
+}
+
+
 ISR(TIMER1_COMPA_vect) {
     // Start ADC conversion on the current channel
     ADMUX = (ADMUX & 0xF8) | current_channel;
@@ -63,7 +75,14 @@ ISR(ADC_vect) {
 }
 
 
-// Read the target speed from the UART and update rps_target
+// Interrupt Service Routine for INT0 (external interrupt 0)
+ISR(INT0_vect) {
+    uint8_t rotation_time = millis - last_rotation;
+    last_rotation = millis;
+    rps_led = 1000 / rotation_time;
+}
+
+
 int8_t read_current_speed(void) {
   static char buff[4];                    // Buffer to hold the incoming string (max 3 digits + null terminator)
   static uint8_t index = 0;
@@ -72,15 +91,15 @@ int8_t read_current_speed(void) {
     char c = uart_getchar(NULL);
     if (c == '\n') {
       buff[index] = '\0';                 // Null-terminate the string
-      rps = (float)atoi(buff);         // Convert string to uint16_t
+      rps_esc = (float)atoi(buff);        // Convert string to uint16_t
       index = 0;                          // Reset buff index
-      return 1;                           // rps_target updated
+      return 1;                           // rps_esc updated
     } else if (index < sizeof(buff) - 1) {
       buff[index] = c;                    // Add character to buff
       index++;                            // Increment buff index
     }
   }
-  return 0;                               // rps_target not updated
+  return 0;                               // rps_esc not updated
 }
 
 
@@ -91,6 +110,13 @@ int main(void) {
 
   // Set PD3 as output
   DDRD |= (1 << PD3);
+
+  // Set IR LED pin as output
+  DDRB |= (1 << IR_LED_PIN);
+  PORTB |= (1 << IR_LED_PIN);
+
+  // Init led fallung edge interrupt
+  interrupt_init();
 
   // Initialize the ADC
   adc_init();
@@ -110,7 +136,7 @@ int main(void) {
     float power = voltage * current;
 
     if (millis % 500 == 0) {
-      printf("%.2f V, %.2f A, %.2f W, RPS: %.2f\n", voltage, current, power, rps);
+      printf("%.2f V, %.2f A, %.2f W, RPS_ESC: %d, RPS_LED: %d\n", voltage, current, power, rps_esc, rps_led);
     }
     
     if (current > OVERCURRENT_THRESHOLD || voltage < UNDERVOLTAGE_THRESHOLD) {
